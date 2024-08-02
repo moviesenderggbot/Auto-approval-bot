1533298c9e2209d2c26d80e84b816fee2353a536
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-02 22:46:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     API_ID = int(getenv("API_ID", "22620068"))
     API_HASH = getenv("API_HASH", "11e2c113078324f7e36688baa86c3911")
     BOT_TOKEN = getenv("BOT_TOKEN", "7025776766:AAGB2MX1-TCinP1okw-aJAgALwZh9pmPZdU")
-    FSUB = getenv("FSUB", "zoyamoviesbotupdate")
+    FSUB = getenv("FSUB", "MX_FLIX")
     CHID = int(getenv("CHID", "-1002050604893"))
     SUDO = list(map(int, getenv("SUDO", "6062527012").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://0qxwx42ukf:oxfFP9wSRVYt7awV@cluster0.abjqxfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
