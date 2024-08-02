dd894ca579813bd51027f2fcd2e16a718a07113f
moviesenderggbot <157599470+moviesenderggbot@users.noreply.github.com>
2024-08-02 23:23:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config:
     API_HASH = getenv("API_HASH", "11e2c113078324f7e36688baa86c3911")
     BOT_TOKEN = getenv("BOT_TOKEN", "7025776766:AAGB2MX1-TCinP1okw-aJAgALwZh9pmPZdU")
     FSUB = getenv("FSUB", "MX_FLIX")
-    CHID = int(getenv("CHID", "-1002050604893"))
+    CHID = int(getenv("CHID", "-1002174170758"))
     SUDO = list(map(int, getenv("SUDO", "6062527012").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://0qxwx42ukf:oxfFP9wSRVYt7awV@cluster0.abjqxfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
